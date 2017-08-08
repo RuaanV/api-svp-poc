4f47e65173d94ba0d53fa5d9ce6d1a3c3fc9c58c
RuaanV <ruaan.venter@river-island.com>
2017-08-08 13:43:07 +0100

    Local updates
    Updated to get working locally

--- go-server/go/skus.go
+++ go-server/go/skus.go
@@ -1,20 +1,18 @@
-package 
+package blah
 
 import (
 	"net/http"
 )
 
 type Skus struct {
-
 }
 
 func GetSkuById(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-		w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
 }
 
 func ListSkus(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-		w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
 }
-
