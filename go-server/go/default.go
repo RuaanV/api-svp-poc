5b5a9ad81772d06c29ce592ba9a68ee750815773
RuaanV <ruaan.venter@river-island.com>
2017-08-23 09:43:19 +0100

    Demo for go
    Branch not to be merged into main

--- go-server/go/default.go
+++ go-server/go/default.go
@@ -1,11 +1,11 @@
-package 
+package blah
 
 import (
 	"net/http"
 )
 
+//Default - method to write back response of unreconised
 type Default struct {
-
 }
 
 //CreateSkuCommand created SKU via API putting it onto a queue
@@ -14,8 +14,8 @@ func CreateSkuCommand(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusAccepted)
 }
 
+//PingGet - test api heart beat
 func PingGet(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-		w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
 }
-
